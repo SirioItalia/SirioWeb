73b64b6ad9567e938e2fa0570bbbdb93e71f5255
laxan <laxan94000@gmail.com>
2022-06-28 22:57:21 +0200

    move front to another repo

--- next-front-app/pages/index.js
+++
@@ -1,9 +0,0 @@
-import SetPosts from "@components/Posts/PostSet"
-import useApi from "src/hooks/useApi"
-
-const Home = () => {
-  const [err, data] = useApi("get", "/users")
-  console.log(data)
-  return <div></div>
-}
-export default Home
